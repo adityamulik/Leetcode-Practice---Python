043318175fc2e3fa803145e0d4bc91eb7909060a
adityamulik <aditya.mulik@gmail.com>
2021-03-13 06:33:55 +0530

    two_sums

--- two_sum.py
+++ two_sum.py
@@ -8,8 +8,19 @@ def twoSum(nums, target):
         temp = i
         k = i+1
         li = []
-        while k < len(nums) and nums[temp]+nums[k] != target:
+        while k < len(nums):
+            if nums[temp]+nums[k] == target:
+                li.append(i)
+                li.append(k)
+                return li
             k +=1
-            return i, k
+
+# Approach 2
+# def twoSum(nums, target):
+#     for indx, num in enumerate(nums[:-1]):
+#         for indx2, num2 in enumerate(nums[indx+1:]):
+#             if num + num2 == target:
+#                 return [indx, indx+indx2+1]
+
 
 print(twoSum(numbers, target))
